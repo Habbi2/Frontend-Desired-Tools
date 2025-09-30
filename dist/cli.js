8c104e29a350365206fc3692ed1bf17f1a228e73
Habbi413 <javieralbertoni@gmail.com>
2025-09-30 03:28:32 -0300

    feat(cli): add CLI entry and bin config for tools-portal

---
+++ dist/cli.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+"use strict";
+console.log('Tools Portal CLI: Hello from tools-portal!');
---
+++ src/cli.ts
@@ -0,0 +1,2 @@
+#!/usr/bin/env node
+console.log('Tools Portal CLI: Hello from tools-portal!');
